08987866fd63a7ca5c3396bed7348be4c32b5016
Joao Brito <machado000@gmail.com>
2025-07-28 10:07:06 -0300

    Minor adjusts

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,17 +1,27 @@
-import os
-from google_sheets_helper.client import GoogleSheetsHelper
+"""
+Basic Google Sheets Helper Example
 
-# Path to your service account credentials
-CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__), '../secrets/client_secret.json')
+This example demonstrates the basic usage of the google-sheets-helper package
+to extract data from Google Sheets and export it to a parquet file with database-like schema.
+"""
+import logging
+import pandas as pd  # noqa
+# import parquet
+
+from google_sheets_helper import GoogleSheetsHelper, load_client_secret, setup_logging
 
 
 if __name__ == "__main__":
 
+    setup_logging(level=logging.INFO)
+
+    # credentials_path = os.path.join(os.path.dirname(__file__), '../secrets/client_secret.json')
+    client_secret = load_client_secret()
+    helper = GoogleSheetsHelper(client_secret)
+
     # Spreadsheet and worksheet info
     spreadsheet_id = "1KurBS2TTaWsDvR9aNGkqMtSgaDyBvKW8"
     worksheet_name = "Receita"
-
-    helper = GoogleSheetsHelper(credentials_path=CREDENTIALS_PATH)
 
     df = helper.read_sheet_to_df(spreadsheet_id, worksheet_name)
 
